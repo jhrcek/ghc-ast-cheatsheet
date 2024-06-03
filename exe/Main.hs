a7268c914d61cf30cbf86563ae0eabc74d57e7d3
Jan Hrček <honza.hrk@gmail.com>
2024-06-03 18:40:05 +0200

    Examples of Pat

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,8 +1,15 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import Language.Haskell.GHC.ExactPrint.Utils (showAst)
+import Language.Haskell.GHC.ExactPrint (parseModule)
+import GHC.Paths (libdir)
+import GHC.Utils.Outputable
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  res <- parseModule libdir "src/Language/Haskell/Syntax/Pat/Example.hs"
+  case res of
+    Left err -> putStrLn $ showSDocUnsafe $ ppr err
+    Right parsedSource ->
+      putStrLn $ showAst parsedSource
+
---
+++ src/Language/Haskell/Syntax/Pat/Example.hs
@@ -0,0 +1,35 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE UnboxedSums #-}
+{-# LANGUAGE TypeAbstractions #-}
+{-# LANGUAGE NPlusKPatterns #-}
+module Language.Haskell.Syntax.Pat.Example where
+import Data.String
+wildPat _ {- WildPat -} = ()
+varPat x {- VarPat -} = x
+lazyPat ~x {- LazyPat -} = x
+asPat x@y {- AsPat -} = (x,y)
+parPat (x) {- ParPat -} = x
+bangPat !x {- BangPat -} = x
+listPat [x, y] {- ListPat -} = (x, y)
+tuplePat (x, y) {- TuplePat -} = (x, y)
+-- Requires -XUnboxedSums
+sumPat (# x | #) {- SumPat -} = x
+conPat (ConPatDemo x y) {- ConPat -}= (x, y)
+viewPat (id -> x) {- ViewPat -}= x
+splicePat $( [p| x |] ) {- SplicePat -} = x
+litPat 'x' {- LitPat -} = 'x'
+nPat 42 {- NPat -} = ()
+nPlusKPat (k+1) {- NPlusKPat -} = k
+
+sigPat (x :: Int) {- SigPat -} = x
+
+invisPat :: forall a. a -> a
+invisPat @t {- InvisPat -} x = x :: t
+
+
+
+-- Helpers
+data ConPatDemo = ConPatDemo Int Bool
